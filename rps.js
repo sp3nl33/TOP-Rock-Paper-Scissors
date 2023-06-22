b34cbdaa2d17883d4c1ebcb318f4a578455ee4b7
sp3nl33 <sp3nl33@gmail.com>
2023-06-22 15:30:16 -0400

    Add win and lose functions, play round functions
    This commit adds functions that define how a user wins or loses against
    the computer, as well automating how a round will play out.
    
    Additionally, it adds a counter to keep the score between the computer
    and the user. There are some things regarding returning values and
    having values that will be used within the parenthesis that I'm still a
    little lost on, so I will definitely need to review those concepts
    again.
    
    For now, it works...

--- rps.js
+++ rps.js
@@ -6,44 +6,121 @@
 //At the end of the round, have a message appear saying whether or not the user won and what the result was
 //Write a function that stops after five rounds and announces the winner and score at the end
 
-let p1
-let cpu
-let p1Score
-let cpuScore
+let playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+let p1Score=0
+let cpuScore=0
 
 
-const rock="Rock"
-const scissors="Scissors"
-const paper="Paper"
+const rock='rock'
+const scissors='scissors'
+const paper='paper'
 
+
+//Choice functions
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 function getComputerChoice() {
-    let cpu = getRandomInt(3);
-    if (cpu===0){
-        let cpu=rock
-        return cpu
+    let computerSelection = getRandomInt(3);
+    if (computerSelection===0){
+        let computerSelection="rock"
+        console.log(computerSelection)
+        console.log(typeof computerSelection)
+        return computerSelection   
     }
-    else if (cpu===1){
-        let cpu=scissors
-        return cpu
+    else if (computerSelection===1){
+        let computerSelection="scissors"
+        console.log(computerSelection)
+        console.log(typeof computerSelection)
+        return computerSelection   
     }
 
-    else if (cpu===2){
-        let cpu=paper
-        return cpu
-    }   
+    else if (computerSelection===2){
+        let computerSelection="paper"
+        console.log(computerSelection)
+        console.log(typeof computerSelection)
+        return computerSelection   
+        
+    }
 }
 
 function getPlayerChoice() {
-    let p1 = prompt("Pick your weapon, warrior -- Rock, Paper, or Scissors?");
-    return p1
+    let pChoice = prompt("Pick your weapon, warrior -- Rock, Paper, or Scissors?");
+    let playerSelection=pChoice.toLocaleLowerCase()
+    String(playerSelection)
+    return playerSelection
+}
+//
+
+//Win and lose functions
+function rockWin() {
+    alert("Player chose rock and computer chose scissors! You win this round!")
+    p1Score++
 }
 
-getComputerChoice();
-getPlayerChoice();
+function scissorsWin() {
+    alert("Player chose scissors and computer chose paper! You win this round!")
+    p1Score++
+}
+
+function paperWin() {
+    alert("Player chose paper and computer chose rock! You win this round!")
+    p1Score++
+}
+
+function rockLose() {
+    alert("Player chose rock and computer chose paper! You lose this round...")
+    cpuScore++
+}
+
+function scissorsLose() {
+    alert("Player chose scissors and computer chose rock! You lose this round...")
+    cpuScore++
+}
+
+function paperLose() {
+    alert("Player chose paper and computer chose scissors! You lose this round...")
+    cpuScore++
+}
+
+function drawRound() {
+    alert("Player and computer chose the same weapon! It's a draw.")
+    p1Score++
+    cpuScore++
+}
+//
+
+//Round play function
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection==="rock" && computerSelection==="scissors") {
+        rockWin();
+    }
+    else if (playerSelection==="scissors" && computerSelection==="paper") {
+        scissorsWin();
+    }
+    else if (playerSelection==="paper" && computerSelection==="rock") {
+        paperWin();
+    }
+    else if (playerSelection==="rock" && computerSelection==="paper") {
+        rockLose();
+    }
+    else if (playerSelection==="scissors" && computerSelection==="rock") {
+        scissorsLose();
+    }
+    else if (playerSelection==="paper" && computerSelection==="scissors") {
+        paperLose();
+    }
+    else if(playerSelection===computerSelection) {
+        drawRound();
+    }
+    else{
+        alert("error")
+    }
+}
+
+playRound(playerSelection, computerSelection)
 
 
 
