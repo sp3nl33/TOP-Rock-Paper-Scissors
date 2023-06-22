cba3ffc7d8257e5e5f0a008b9b6db8c75e03c1b4
sp3nl33 <sp3nl33@gmail.com>
2023-06-22 16:36:59 -0400

    Add game function
    This commit adds the game function, which runs five rounds. After five
    rounds, the browswer displays an alert and lets the user know if they
    won or lost with the final score at the end.
    
    There's plenty I still don't understand completely, but I know I will be
    returning to this project.

--- rps.js
+++ rps.js
@@ -6,8 +6,7 @@
 //At the end of the round, have a message appear saying whether or not the user won and what the result was
 //Write a function that stops after five rounds and announces the winner and score at the end
 
-let playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+
 let p1Score=0
 let cpuScore=0
 
@@ -26,21 +25,15 @@ function getComputerChoice() {
     let computerSelection = getRandomInt(3);
     if (computerSelection===0){
         let computerSelection="rock"
-        console.log(computerSelection)
-        console.log(typeof computerSelection)
         return computerSelection   
     }
     else if (computerSelection===1){
         let computerSelection="scissors"
-        console.log(computerSelection)
-        console.log(typeof computerSelection)
         return computerSelection   
     }
 
     else if (computerSelection===2){
         let computerSelection="paper"
-        console.log(computerSelection)
-        console.log(typeof computerSelection)
         return computerSelection   
         
     }
@@ -55,73 +48,100 @@ function getPlayerChoice() {
 //
 
 //Win and lose functions
-function rockWin() {
-    alert("Player chose rock and computer chose scissors! You win this round!")
-    p1Score++
-}
+//function rockWin() {
+//    alert("Player chose rock and computer chose scissors! You win this round!")
+//   p1Score++
+//}
 
-function scissorsWin() {
-    alert("Player chose scissors and computer chose paper! You win this round!")
-    p1Score++
-}
+//function scissorsWin() {
+//   alert("Player chose scissors and computer chose paper! You win this round!")
+//    p1Score++
+//}
 
-function paperWin() {
-    alert("Player chose paper and computer chose rock! You win this round!")
-    p1Score++
-}
+//function paperWin() {
+//    alert("Player chose paper and computer chose rock! You win this round!")
+//    p1Score++
+//}
 
-function rockLose() {
-    alert("Player chose rock and computer chose paper! You lose this round...")
-    cpuScore++
-}
+//function rockLose() {
+//    alert("Player chose rock and computer chose paper! You lose this round...")
+//    cpuScore++
+//}
 
-function scissorsLose() {
-    alert("Player chose scissors and computer chose rock! You lose this round...")
-    cpuScore++
-}
+//function scissorsLose() {
+//    alert("Player chose scissors and computer chose rock! You lose this round...")
+//    cpuScore++
+//}
 
-function paperLose() {
-    alert("Player chose paper and computer chose scissors! You lose this round...")
-    cpuScore++
-}
+//function paperLose() {
+//    alert("Player chose paper and computer chose scissors! You lose this round...")
+//    cpuScore++
+//}
 
-function drawRound() {
-    alert("Player and computer chose the same weapon! It's a draw.")
-    p1Score++
-    cpuScore++
-}
+//function drawRound() {
+//    alert("Player and computer chose the same weapon! It's a draw.")
+//    p1Score++
+//    cpuScore++
+//}
 //
 
 //Round play function
 function playRound(playerSelection, computerSelection) {
     if (playerSelection==="rock" && computerSelection==="scissors") {
-        rockWin();
+        console.log("Player chose rock and computer chose scissors! You win this round!");
+        p1Score++
     }
     else if (playerSelection==="scissors" && computerSelection==="paper") {
-        scissorsWin();
+        console.log("Player chose scissors and computer chose paper! You win this round!");
+        p1Score++
     }
     else if (playerSelection==="paper" && computerSelection==="rock") {
-        paperWin();
+        console.log("Player chose paper and computer chose rock! You win this round!");
+        p1Score++
     }
     else if (playerSelection==="rock" && computerSelection==="paper") {
-        rockLose();
+        console.log("Player chose rock and computer chose paper! You lose this round...");
+        cpuScore++
     }
     else if (playerSelection==="scissors" && computerSelection==="rock") {
-        scissorsLose();
+        console.log("Player chose scissors and computer chose rock! You lose this round...");
+        cpuScore++
     }
     else if (playerSelection==="paper" && computerSelection==="scissors") {
-        paperLose();
+        console.log("Player chose paper and computer chose scissors! You lose this round...");
+        cpuScore++
     }
     else if(playerSelection===computerSelection) {
-        drawRound();
+        console.log("Player and computer chose the same weapon! It's a draw.");
+        p1Score++
+        cpuScore++
     }
     else{
         alert("error")
     }
 }
 
-playRound(playerSelection, computerSelection)
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
 
+function game() {
+    playRound(getPlayerChoice(), getComputerChoice()); 
+    playRound(getPlayerChoice(), getComputerChoice());
+    playRound(getPlayerChoice(), getComputerChoice());
+    playRound(getPlayerChoice(), getComputerChoice());
+    playRound(getPlayerChoice(), getComputerChoice());
+    if (cpuScore>p1Score) {
+        alert(`You lose! Final score: CPU: ${cpuScore} to P1: ${p1Score}`)
+    }
+    else if (p1Score>cpuScore) {
+        alert(`You win! Final score: P1: ${p1Score} to CPU: ${cpuScore}`)
+    }
+    else {
+        alert("UNREAL! It's a tie!")
+    }
+}
+
+game()
 
 
 
