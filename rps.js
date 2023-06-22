b74defcd6c75f7d37024ed14dd7fd944544b8bd4
sp3nl33 <sp3nl33@gmail.com>
2023-06-22 00:35:32 -0400

    Add script to HTML, add choice functions
    This commit adds the actual game script to the HTML file. While the
    element was in the file, the actual script that contains all of the
    necessary information to run the game was not in yet.
    
    This commit also has additions to the javascript file, notably, the
    functions to get a random CPU selection, as well as a prompt to have
    the player select their choice. I have a feeling these are going to need
    some massive changes before the script is finished, but it works for
    now.
    
    That's enough javascript for the night.

--- rps.js
+++ rps.js
@@ -1,8 +1,50 @@
-/Establish a game of rock paper scissors/
-/Establish two players, one is a user and the other is a computer/
-/Establish a function to get the player's choice, and a function to get the computer's choice (decided randomly)/
-/Write a function that has the two choices go against each other in a game/
-/Establish rock beats scissors, scissors beats paper, paper beats rock/
-/At the end of the game, have a message appear saying whether or not the user won and what the result was/
-/Write a function that stops after five games and announces the winner and score at the end/
+//Establish two players, one is a user and the other is a computer
+//Establish a function to get the player's choice, and a function to get the computer's choice (decided randomly)
+//Establish a round of rock paper scissors
+//Write a function that has the two choices go against each other in a round
+//Establish rock beats scissors, scissors beats paper, paper beats rock
+//At the end of the round, have a message appear saying whether or not the user won and what the result was
+//Write a function that stops after five rounds and announces the winner and score at the end
+
+let p1
+let cpu
+let p1Score
+let cpuScore
+
+
+const rock="Rock"
+const scissors="Scissors"
+const paper="Paper"
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    let cpu = getRandomInt(3);
+    if (cpu===0){
+        let cpu=rock
+        return cpu
+    }
+    else if (cpu===1){
+        let cpu=scissors
+        return cpu
+    }
+
+    else if (cpu===2){
+        let cpu=paper
+        return cpu
+    }   
+}
+
+function getPlayerChoice() {
+    let p1 = prompt("Pick your weapon, warrior -- Rock, Paper, or Scissors?");
+    return p1
+}
+
+getComputerChoice();
+getPlayerChoice();
+
+
+
 
