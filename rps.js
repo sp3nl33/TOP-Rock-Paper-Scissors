96c105e4b11af83847ebeb6966c32ef5fbbc1a21
sp3nl33 <sp3nl33@gmail.com>
2023-06-21 23:17:12 -0400

    Make html and js file
    This commit establishes the html and javascript files for the project.
    The javascript folder contains some psuedocode that has all of the very,
    VERY basic functions and steps that will need to be made for the
    project. I probably missed a couple of steps here and there, but this is
    a good outline for what needs to be done.

---
+++ rps.js
@@ -0,0 +1,8 @@
+/Establish a game of rock paper scissors/
+/Establish two players, one is a user and the other is a computer/
+/Establish a function to get the player's choice, and a function to get the computer's choice (decided randomly)/
+/Write a function that has the two choices go against each other in a game/
+/Establish rock beats scissors, scissors beats paper, paper beats rock/
+/At the end of the game, have a message appear saying whether or not the user won and what the result was/
+/Write a function that stops after five games and announces the winner and score at the end/
+
